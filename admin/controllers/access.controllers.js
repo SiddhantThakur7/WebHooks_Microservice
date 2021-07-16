10d9d5ebb69ea224e13e7c4b06a0523933ca99c9
Siddhant <sid@Siddhants-MacBook-Pro.local>
2021-07-17 00:30:33 +0530

    Admin: Added a user model to the Models/ directory. Completed login and signup controllers using bcrypt.

--- admin/controllers/access.controllers.js
+++ admin/controllers/access.controllers.js
@@ -1,4 +1,7 @@
-
+const User = require('../Models/users.models');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const pass_text = process.env.PASS;
 
 exports.getLoginPage = (req, res, next) => {
     res.render('access');
@@ -7,12 +10,43 @@ exports.getLoginPage = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
-    console.log(username, password);
+    User.findOne({"username": username})
+    .then((found) => {
+        if(!found){
+            res.redirect('/')
+        }
+        return bcrypt.compare(password, found.password)
+    })
+    .then((match) => {
+        if(!match){
+            res.redirect('/')
+        }
+        res.send('Logged In')
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
     const con_password = req.body.con_password;
-    console.log(username, password, con_password);
+    if(con_password != password){
+        res.redirect('/');
+    }
+    User.findOne({'username': username})
+    .then((found) => {
+        if(found){
+            res.redirect('/');
+        }
+        return bcrypt.hash(password, saltRounds)
+    })
+    .then((hash) => {
+        const new_user = User({
+            username: username,
+            password: hash
+        });
+        return new_user.save()
+    })
+    .then(() => res.send('Signed Up!'))
+    .catch((err) => console.log(err));
 };
